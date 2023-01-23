5ebe89e881f45c074409a344ca5c72b0b1a9656c
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-01-23 15:17:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,8 +47,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Doraemon")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shorturllink.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '95a8195c40d31e0c3b6baa68813fcecb1239f2e9')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gplinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'dac861e077d89e423386b02dc53c59c53092605a')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
